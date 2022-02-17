b1452c9953a359b5dd0de9beab82dc1580aadd7f
Kazuki Yashima <yashima@step1ku.kugi.kyoto-u.ac.jp>
2022-02-17 21:22:49 +0900

    config(kari)

---
+++ yashima/config.py
@@ -0,0 +1,6 @@
+import os
+CONFIG={'local_data_dir_wdc':'iugonet/wdc_kyoto/geomag/','remote_data_dir_wdc':'http://wdc-data.iugonet.org/data/'}
+
+
+if os.environ.get('SPEDAS_DATA_DIR') :
+    CONFIG['local_data_dir_wdc']=os.sep.join([os.environ['SPEDAS_DATA_DIR'],'iugonet/wdc_kyoto/geomag/'])
