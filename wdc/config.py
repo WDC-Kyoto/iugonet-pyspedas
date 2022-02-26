1e75f4eb5d83b572aaf3663dbb222bf5ebaa0dde
HarutoKoike <koike@kugi.kyoto-u.ac.jp>
2022-02-27 01:54:59 +0900

    2022-02-26

--- wdc/config.py
+++ wdc/config.py
@@ -1,18 +1,15 @@
-
-
 import os
 
-CONFIG = {'local_data_dir_qddays': 'iugonet_data/qddays', 
-					'local_data_dir_wp_index':'iugonet_data/wp_index',
+CONFIG = {'local_data_dir_qddays': 'iugonet/wdc/qddays', 
+					'local_data_dir_wp_index':'iugonet/wdc/wp_index',
 					'remote_data_dir_qddays':'http://wdc-data.iugonet.org/',  
 					'remote_data_dir_wp_index':'https://www.isee.nagoya-u.ac.jp/~nose.masahito/s-cubed/data/'}
 
 
-
 if os.environ.get('SPEDAS_DATA_DIR') :
-	CONFIG['local_data_dir_qddays'] = os.sep.join(\
-														        [os.environ['SPEDAS_DATA_DIR'], \
-														        'iugonet/qddays'])
+	CONFIG['local_data_dir_qddays']   = os.sep.join(\
+														          [os.environ['SPEDAS_DATA_DIR'], \
+														           'iugonet', 'qddays'])
 	CONFIG['local_data_dir_wp_index'] = os.sep.join(\
 														         [os.environ['SPEDAS_DATA_DIR'], \
-														         'iugonet/wp_index'])
+														         'iugonet', 'wp_index'])
--- wdc/read.py
+++ wdc/read.py
@@ -1,23 +1,58 @@
 
 
-import load
+import download
 from html.parser import HTMLParser
 
 
 
 ### qddays ###
-def qddays(trange=['2011-1-1', '2013-1-1']):
+def qddays(trange=['2011-1-1', '2011-1-1']):
 	#
-	local_files = load.qddays(trange)
+	local_files = download.qddays(trange)
   #
-	
-	for lf in local_files:
-		with open(lf) as f:
-			line = f.readline()
-			while line:
-				line = line.rstrip()
-				print(line)
-				line = f.readline()
+	yr   = []
+	mon  = []
+	q1   = []
+	q2   = []
+	q3   = []
+	q4   = []
+	q5   = []
+	q6   = []
+	q7   = []
+	q8   = []
+	q9   = []
+	q0   = []
+	d1   = []
+	d2   = []
+	d3   = []
+	d4   = []    
+	d5   = []    
+
+	for lf in local_files :
+		with open(lf) as f :
+			data = f.readlines()
+			# delete "\n"
+			data = [d.replace('\n', '') for d in data] 
+			for i in range(len(data)) :
+				data0 = data[i]
+				yr.append(data0[0:4])
+				mon.append(data0[5:7])
+				q1.append(data0[8:10]) 
+				q2.append(data0[10:12]) 
+				q3.append(data0[12:14]) 
+				q4.append(data0[14:16]) 
+				q5.append(data0[16:18]) 
+				q6.append(data0[19:21]) 
+				q7.append(data0[21:23]) 
+				q8.append(data0[23:25]) 
+				q9.append(data0[25:27]) 
+				q0.append(data0[27:29]) 
+				d1.append(data0[30:32])
+				d2.append(data0[32:34])
+				d3.append(data0[34:36])
+				d4.append(data0[36:38])
+				d5.append(data0[38:40])  
+				
 	return 0
 
 
@@ -25,7 +60,7 @@ def qddays(trange=['2011-1-1', '2013-1-1']):
 ### wp_index ###
 def wp_index(trange=['2011-1-1', '2011-1-2']):
 	#
-	local_files = load.wp_index(trange)
+	local_files = download.wp_index(trange)
 	for lf in local_files:
 		with open(lf) as f:
 			d = f.read()
@@ -33,5 +68,9 @@ def wp_index(trange=['2011-1-1', '2011-1-2']):
 	return 0
 
 
-a = wp_index()
+a = qddays()
+
+
+
+
 			
