904b0df7f85a605a97f82c31a2333a11da92d57c
Kazuki Yashima <yashima@step1ku.kugi.kyoto-u.ac.jp>
2022-03-20 17:01:54 +0900

    last_ver

--- yashima/config.py
+++ yashima/config.py
@@ -1,6 +1,12 @@
 import os
-CONFIG={'local_data_dir_wdc':'iugonet/wdc_kyoto/geomag/','remote_data_dir_wdc':'http://wdc-data.iugonet.org/data/'}
+CONFIG={'local_data_dir_dst':'',
+        'local_data_dir_site':'',
+        'remote_data_dir_dst':'http://wdc-data.iugonet.org/data/hour/index/',
+       'remote_data_dir_site':'http://wdc-data.iugonet.org/data/',}
 
-
+root_dir=''
 if os.environ.get('SPEDAS_DATA_DIR') :
-    CONFIG['local_data_dir_wdc']=os.sep.join([os.environ['SPEDAS_DATA_DIR'],'iugonet','wdc_kyoto','geomag'])
+    root_dir = os.environ.get('SPEDAS_DATA_DIR')
+
+    CONFIG['local_data_dir_dst']=os.sep.join([root_dir,'iugonet','wdc_kyoto','dst'])
+    CONFIG['local_data_dir_site']=os.sep.join([root_dir,'iugonet','wdc_kyoto','site'])
