a83d1ec6e296dcd6abbe963637cc9ead34bba147
Kazuki Yashima <yashima@step1ku.kugi.kyoto-u.ac.jp>
2022-06-21 17:15:14 +0900

    for ack

--- iugonet_wdc/iug_load_gmag_wdc.py
+++ iugonet_wdc/iug_load_gmag_wdc.py
@@ -1,32 +1,38 @@
-from wdc import site as st
-
+from .wdc.site import site as st
+from .iug_load_gmag_wdc_acknowledgement import iug_wdc_ack as ack
 def iug_load_gmag_wdc(trange=['2011-1-1', '2011-1-2'],level="final",site="dst kak ae asy sym",res="hour"):
     site2=site.split(" ")
     if ("dst" in site2):
-        from wdc import dst
-        dst.dst(trange=trange,level=level)
+        from .wdc.dst import dst
+        dst(trange=trange,level=level)
+        ack("dst")
     if("ae" in site2):
-        from wdc import ae
-        ae.ae(trange=trange,level=level,res=res)
+        from .wdc.ae import ae
+        ae(trange=trange,level=level,res=res)
+        ack("ae")
     if("asy" in site2):
-        from wdc import asy
-        asy.asy(trange=trange)
+        from .wdc.asy import asy
+        asy(trange=trange)
+        ack("asy")
     if("sym" in site2):
-        from wdc import sym
-        sym.sym(trange=trange)
+        from .wdc.sym import sym
+        sym(trange=trange)
+        ack("sym")
     site2=[s for s in site2 if not((s=="dst")or(s=="ae")or(s=="sym")or(s=="asy"))]
     for ss in site2:
-        st.site(trange=trange,res=res,site=ss)
+        st(trange=trange,res=res,site=ss)
     return 0
 
 def iug_load_gmag_wdc_qddays(trange=['2011-1-1', '2011-1-2']):
-    from wdc import qddays
-    result=qddays.qddays(trange=trange)
+    from .wdc.qddays import qddays
+    result=qddays(trange=trange)
+    ack("qddays")
     return result
 
 def iug_load_gmag_wdc_wp_index(trange=['2010-1-1', '2010-1-2']):
-    from wdc import wp_index
-    wp_index.wp_index(trange=trange)
+    from .wdc.wp_index import wp_index
+    wp_index(trange=trange)
+    ack("wp_index")
     return 0
 
 #(1)
