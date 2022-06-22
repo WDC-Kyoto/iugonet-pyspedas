c498a1f3d2511ca91e7f4824cf1ddbb64ce6ea72
kazukiyashima <95340194+kazukiyashima@users.noreply.github.com>
2022-06-22 15:13:00 +0900

    Update iug_load_gmag_wdc.py
    fix

--- iugonet_wdc/iug_load_gmag_wdc.py
+++ iugonet_wdc/iug_load_gmag_wdc.py
@@ -19,19 +19,11 @@ def iug_load_gmag_wdc(trange=['2011-1-1', '2011-1-2'],level="final",site="dst ka
         from .wdc.sym import sym
         sym(trange=trange)
         ack("sym")
-    if("ae" in site2):
-        from .wdc.ae import ae
-        ae(trange=trange,level=level,res=res)
-    if("asy" in site2):
-        from .wdc.asy import asy
-        asy(trange=trange)
-    if("sym" in site2):
-        from .wdc.sym import sym
-        sym(trange=trange)
 
     site2=[s for s in site2 if not((s=="dst")or(s=="ae")or(s=="sym")or(s=="asy"))]
     for ss in site2:
         st(trange=trange,res=res,site=ss)
+    ack("site")
     return 0
 
 def iug_load_gmag_wdc_qddays(trange=['2011-1-1', '2011-1-2']):
